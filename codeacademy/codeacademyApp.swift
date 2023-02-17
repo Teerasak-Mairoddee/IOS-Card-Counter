c29bfd507da5c74a83d1d77c0810dbb21cbe39d3
teerasakmairoddee556 <115946374+teerasakmairoddee556@users.noreply.github.com>
2023-02-17 00:31:36 +0000

    Initial Commit

---
+++ codeacademy/codeacademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeacademyApp.swift
+//  codeacademy
+//
+//  Created by Teerasak Mairoddee on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct codeacademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
